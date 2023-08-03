9c2fa144fb4e9101305eeb1dddf5c79d5c9e56d6
20IVA <1546361@senati.pe>
2023-08-03 12:45:47 -0500

    curso de personas

--- cursopersona.py
+++ cursopersona.py
@@ -1,9 +1,13 @@
-class persona:
+class Persona:
     def __init__(self, nombre, genero, altura, edad):
        self._nombre = nombre
        self._genero = genero
        self._altura = altura
        self._edad = edad
+
+    def edad(self):
+        self._fecha_nacimiento
+        return 15
        
 
     def comer(self, hora_comer):
@@ -18,11 +22,26 @@ class persona:
         print(f"{self._nombre} enpieza a estudiar{tienpo_estudiar}") 
         print("terminando de estudiar puedes jugar ")    
 
-hector = persona("hector", "honbre", "1.76","19")   
+hector = Persona("hector", "honbre", "1.76","19")   
 hector.comer ("en la mesa")
 
-katy = persona("katy", "femenino", "1.50","15") 
+katy = Persona("katy", "femenino", "1.50","15") 
 katy.jugar ("1 hora")
 
-celeste = persona("celeste", "femenino", "1.67","12") 
+celeste = Persona("celeste", "femenino", "1.67","12") 
 celeste.estudiar ("3 horas")
+
+
+
+
+
+class alumno(Persona):
+    def __inint__(self, nombre, genero, dni, fecha_nacimiento, peso, altura, id_estudiante, email):
+        super().__inint__(nombre, genero, dni, fecha_nacimiento, peso, altura)
+        self._id_estudiante = id_estudiante
+        self._email = email
+
+    def info(self): 
+        print (f"{self._nombre}") 
+
+juan = alumno ("juan", "femenino", "457685834","10/09/2000", "60")
